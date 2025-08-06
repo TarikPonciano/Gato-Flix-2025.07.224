68d3b3e24b928856f920508f0e3a036e6003b8e9
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-08-06 09:37:32 -0300

    Implementação Login e Cadastro Telas

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 # from . import views
-from .views import home
+from .views import home, login, cadastro
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'catalogo/home.html')
+
+def login(request):
+    return render(request, 'catalogo/login.html')
+
+def cadastro(request):
+    return render(request, 'catalogo/cadastro.html')
